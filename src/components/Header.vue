2d04481c52ac51594d45c109e2c2ce3570a073d2
shikachii <shikachii095@gmail.com>
2021-05-09 00:03:49 +0900

    :art: ヘッダーをコンポーネントに分ける

---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <div :class="$style.root">
+    Header
+    {{ uid }}
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/signin">Signin</router-link> |
+    <router-link to="/signup">Signup</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import firebase from 'firebase';
+
+export default Vue.extend({
+  name: 'header',
+  computed: {
+    uid() {
+      return firebase.auth().currentUser.uid;
+    },
+  },
+});
+</script>
+
+<style module>
+.root {
+  padding: 30px;
+  background: #5cb85c;
+}
+
+.root a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+.root a.router-link-exact-active {
+  /* color: #42b983; */
+  color: #fff;
+}
+</style>
