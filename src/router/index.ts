d5931ac067881bf07874adb4635e0759ffb4eedb
shikachii <shikachii095@gmail.com>
2021-05-08 22:43:24 +0900

    新規登録ページとログインページを追加

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '../views/Home.vue';
+
+import Signin from '@/views/Signin.vue';
+import Signup from '@/views/Signup.vue';
+import Home from '@/views/Home.vue';
 
 Vue.use(VueRouter);
 
@@ -17,6 +20,16 @@ const routes: Array<RouteConfig> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path: '/signin',
+    name: 'Signin',
+    component: Signin,
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup,
   },
 ];
 
---
+++ src/views/Signin.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="$style.root">
+    <h2>ログイン</h2>
+    <input type="text" v-model="account.username" />
+    <input type="password" v-model="account.password" />
+    <button @click="signin">送信</button>
+    <div>
+      {{ account.username }}
+    </div>
+    <div>
+      {{ account.password }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+type AccountType = {
+  username: string;
+  password: string;
+};
+
+type DataType = {
+  account: AccountType;
+};
+
+export default Vue.extend({
+  name: 'Signin',
+  data: (): DataType => ({
+    account: {
+      username: '',
+      password: '',
+    },
+  }),
+  methods: {
+    signin() {
+      console.log(`${this.account.username} : ${this.account.password}`);
+    },
+  },
+});
+</script>
+
+<style module>
+</style>
