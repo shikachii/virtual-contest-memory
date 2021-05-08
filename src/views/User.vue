5611ca4724cbe6c8de4cd41329920dc27882a5ef
shikachii <shikachii095@gmail.com>
2021-05-09 00:23:18 +0900

    :sparkles: ログアウト機能を作成

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="$style.root">
     Hello, {{ uid }}!
+    <button @click="logout">ログアウト</button>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import firebase from 'firebase';
 
 export default Vue.extend({
   computed: {
     uid() {
       return this.$route.params.id;
+    },
+  },
+  methods: {
+    async logout() {
+      await firebase.auth().signOut();
+      this.$router.push({ name: 'Home' });
     },
   },
 });
